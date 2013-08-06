07c7302545110151ed9f94925806dae86cd68083
David Estes <davydotcom@gmail.com>
2013-08-05 20:53:36 -0400

    updated handlebars requirement

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
 	plugins {
 
 		runtime ":asset-pipeline:0.2.1"
-		compile ":handlebars-asset-pipeline:1.0.0-RC4.2"
+		compile ":handlebars-asset-pipeline:1.0.0-RC4.3"
 
 		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
