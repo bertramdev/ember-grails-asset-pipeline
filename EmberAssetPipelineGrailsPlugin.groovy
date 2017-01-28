8cded413b15bdd5143030e901396fb9bdee7cbe5
David Estes <davydotcom@gmail.com>
2017-01-28 16:10:35 -0500

    udpating to latest

--- EmberAssetPipelineGrailsPlugin.groovy
+++ EmberAssetPipelineGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import asset.pipeline.handlebars.HandlebarsAssetFile
 import asset.pipeline.ember.EmberHandlebarsProcessor
 class EmberAssetPipelineGrailsPlugin {
 
-    def version = "2.11.0"
+    def version = "2.12.10"
     def grailsVersion = "2.2 > *"
     def title = "Ember.js Asset-Pipeline Plugin"
     def author = "David Estes"
